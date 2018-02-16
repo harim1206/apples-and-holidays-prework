c54e3846afe8f779bcc885c7bd11151e955d3c21
Harim Choi <harim1206@gmail.com>
2018-02-16 23:58:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,14 +86,16 @@ def all_supplies_in_holidays(holiday_hash)
     data.each{
       |holiday, supplies|
 
-      holiday_separate = holiday.to_s.split("_")
-=begin      
+      holiday_separate = holiday.to_s.split("_").map{
+        word.capitalize!
+      }
+=begin
       holiday_separate_capitalized = holiday_separate.map{
         |word|
         word.capitalize!
       }
 =end
-      
+
       puts("holiday_separate: #{holiday_separate}")
       puts("holiday_separate_capitalized: #{holiday_separate_capitalized}")
       #puts ("#{holiday.to_s.capitalize!}: #{supplies.join(", ")}")
